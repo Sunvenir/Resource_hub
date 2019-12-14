8d9e1947516e315f5ff4497e69a8dff59dae8b11
zhaoziyu99 <zhaoziyu99@sohu.com>
2019-12-14 10:54:40 +0800

    ultimate patent

--- src/main/java/com/example/demo/result/PatentDeleteResult.java
+++ src/main/java/com/example/demo/result/PatentDeleteResult.java
@@ -2,8 +2,14 @@ package com.example.demo.result;
 
 public class PatentDeleteResult {
     private int code;
+
     public void setCode(int code)
     {
         this.code = code;
     }
+
+    public int getCode() {
+        return code;
+    }
+
 }
--- src/main/java/com/example/demo/result/PatentSearchResult.java
+++ src/main/java/com/example/demo/result/PatentSearchResult.java
@@ -4,8 +4,17 @@ import com.example.demo.entity.Patent;
 
 public class PatentSearchResult {
     private Patent patent;
+
     public PatentSearchResult(Patent p)
     {
         this.patent = p;
     }
+    public Patent getPatent() {
+        return patent;
+    }
+
+    public void setPatent(Patent patent) {
+        this.patent = patent;
+    }
+
 }
