1ce0654a91d16bf92395d439da9e7fa8fc70f038
Souvenir <604610263@qq.com>
2019-12-14 09:46:18 +0800

    expert add

---
+++ src/main/java/com/example/demo/result/ExpertaddResult.java
@@ -0,0 +1,17 @@
+package com.example.demo.result;
+
+public class ExpertaddResult {
+    int code;
+
+    public ExpertaddResult(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
